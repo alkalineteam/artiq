451dc2e4835bb6bfbcdb7b0de7121be224991b29
mwojcik <mw@m-labs.hk>
2026-04-17 11:18:08 +0800

    tests: adjust batching test count

--- artiq/test/coredevice/test_rtio.py
+++ artiq/test/coredevice/test_rtio.py
@@ -612,7 +612,7 @@ class RTIOBatching(EnvExperiment, Generic[TTLOutCapable]):
         self.core.reset()
         self.core.delay(10.*ms)
         with self.core_batch:
-            for i in range(500):
+            for i in range(100):
                 self.ttl_out.pulse(250.*ns)
                 self.core.delay(50.*ns)
 
