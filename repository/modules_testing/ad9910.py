a0489c3234d0471483a4256ee8524ae6be65dab7
Anurag Borah <anuragborah2548@gmail.com>
2026-05-19 11:24:34 +0100

    AOM connection check

--- repository/modules_testing/ad9910.py
+++ repository/modules_testing/ad9910.py
@@ -6,7 +6,11 @@ class AD9910(EnvExperiment):
     def build(self):
         self.setattr_device("core")
         self.core:Core
-        self.ad9910_0=self.get_device("urukul1_ch0") 
+        self.ad9910_0=self.get_device("urukul0_ch0") 
+
+        self.setattr_argument("Enable_RF", BooleanValue(default=False))
+        self.setattr_argument("Frequency", NumberValue())
+        self.setattr_argument("Attenuation", NumberValue())
 
     @kernel
     def run(self):
@@ -18,8 +22,13 @@ class AD9910(EnvExperiment):
         self.ad9910_0.cpld.init()
         self.ad9910_0.init()
         
-        self.ad9910_0.set_att(0.0)     #limit  : 31.5
-        self.ad9910_0.set(frequency=10 * MHz, amplitude=1.0)
+        self.ad9910_0.set_att(self.Attenuation)    #limit  : -31.5
+        self.ad9910_0.set(frequency=self.Frequency * MHz)
+
+        if self.Enable_RF:
+            self.ad9910_0.sw.on()
+        else:
+            self.ad9910_0.sw.off()
 
         # for i in range(10):
         #     self.ad9910_0.sw.on()
